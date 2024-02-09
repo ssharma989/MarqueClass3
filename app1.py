b0bef1cb863561e93dde9aecfc4a7a4d381294d7
ssharma989 <150552503+ssharma989@users.noreply.github.com>
2024-02-09 14:19:25 -0500

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  9 13:32:15 2024
+
+@author: sharm
+"""
+
+import streamlit as st
+import pandas as pd 
+import plotly.express as px 
+
+df = pd.read_csv('StockData/AAPL.csv', parse_dates=['Date'], index_col=['Date'])
+fig = px.line(df, y='Adj Close')
+st.title("DEMO APP - Schulich Class")
+st.write(df) #print 
+st.plotly_chart(fig)
