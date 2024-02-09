51e6c0cd13f1246e559842bf097899993c6adda5
ssharma989 <150552503+ssharma989@users.noreply.github.com>
2024-02-09 14:40:37 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,8 +9,22 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px 
 
-df = pd.read_csv('AAPL.csv', parse_dates=['Date'], index_col=['Date'])
+
+#Grab Data
+tickers = ['AAPL','DIS','NKE']
+ticker = st.sidebar.selectbox("Pick a ticker:",tickers)
+df = pd.read_csv('StockData/' + ticker + '.csv', parse_dates=['Date'],
+                 index_col=['Date'])
+
+#Filter the data
+year = st.sidebar.selectbox("Pick a year", [2015,2016,2017])
+df = df.loc[str(year)]
+
+
+#Create a chart
 fig = px.line(df, y='Adj Close')
-st.title("DEMO APP - Schulich Class")
-st.write(df) #print 
-st.plotly_chart(fig)
+#Outputs
+st.title("Web App - Schulich Class")
+st.write("Demo example from Training the Street class")
+st.plotly_chart(fig) #output plotly graph
+st.write(df) #similar to print, but will show it in the app
